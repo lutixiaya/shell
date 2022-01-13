fcfdbc4e7a2fff074dec71b4e603d6707ffc7e82
lutixiaya <48750425+lutixiaya@users.noreply.github.com>
2022-01-13 11:09:30 +0800

    更新菜单

--- 系统管理/lutixiaya_check_host_alive.sh
+++ 系统管理/lutixiaya_check_host_alive.sh
@@ -3,15 +3,21 @@
 # site：www.lutixia.cn
 ####################################
 
+check_fping() {
+    echo -e "\t\033[0;32m正在检测并安装fping工具，请稍后...\033[0m"
+    rpm -q fping &>/dev/null && echo -e "\t\033[1;34m检测成功！请继续...\033[0m" || {
+    yum install fping -y &>/dev/null  || echo -e "\t\033[0;31安装fping工具包失败！请检查是否配置好镜像仓库...\033[0m"
+    }
+}
 
 
 check_single_host() {
     read -p "请输入要检测的主机ip,多个ip用空格分隔：" -a  ip
-    echo -e "\t\033[0;32m检测中，请稍后...\033[0m"
+    echo -e "\t\033[0;32m检测中，请稍后...\n---------------------------------------\033[0m"
     fping  -r 1  `echo ${ip[@]}`  &> /tmp/ip_temp_list
-    awk 'BEGIN{printf "%-20s\n","当前在线的ip:"}/alive/{printf "%20s\n",$1}' /tmp/ip_temp_list
-    awk 'BEGIN{printf "\n%-20s\n","当前不在线的ip:"}/unreachable/{printf "%20s\n",$1}' /tmp/ip_temp_list
-    awk 'BEGIN{printf "\n%-20s\n","格式错误的ip:"}/found/{printf "%20s\n",$1}' /tmp/ip_temp_list
+    awk 'BEGIN{printf "%-20s\n","\033[1;36m当前在线的ip:\033[0m"}/alive/{printf "%20s\n",$1}' /tmp/ip_temp_list
+    awk 'BEGIN{printf "\n%-20s\n","\033[1;31m当前不在线的ip:\033[0m"}/unreachable/{printf "%20s\n",$1}' /tmp/ip_temp_list
+    awk 'BEGIN{printf "\n%-20s\n","\033[1;33m格式错误的ip:\033[0m"}/found/{printf "%20s\n",$1}' /tmp/ip_temp_list
     rm -rf /tmp/ip_temp_list
 }
 
@@ -28,8 +34,8 @@ check_multi_host() {
     if  grep "parse" /tmp/ip_temp_list &>/dev/null;then
         echo -e "\t\033[0;31m请参考提示，输入正确格式的ip范围或者网段！\033[0m"
     else
-        awk 'BEGIN{printf "\n%-20s\n","当前不在线的ip:"}/unreachable/{printf "%20s\n",$1}' /tmp/ip_temp_list
-        awk 'BEGIN{printf "%-20s\n","当前在线的ip:"}/alive/{printf "%20s\n",$1}' /tmp/ip_temp_list
+        awk 'BEGIN{printf "\n%-20s\n","\033[1;31m当前不在线的ip:\033[0m"}/unreachable/{printf "%20s\n",$1}' /tmp/ip_temp_list
+        awk 'BEGIN{printf "%-20s\n","\033[1;36m当前在线的ip:\033[0m"}/alive/{printf "%20s\n",$1}' /tmp/ip_temp_list
         rm -rf /tmp/ip_temp_list
     fi
 }
@@ -40,8 +46,8 @@ fping_menu() {
     clear
     echo -e "\t\t\tFping菜单"
     echo -e "\033[0;32m-------------------------------------------------\033[0m"
-    echo -e "\t\t1、检查指定主机是否在线"
-    echo -e "\t\t2、显示局域网主机状态"
+    echo -e "\t\t1、检测指定主机是否在线"
+    echo -e "\t\t2、检测局域网主机状态"
     echo -e "\t\t3、退出"
     echo -e "\033[0;32m-------------------------------------------------\033[0m"
 
@@ -49,10 +55,12 @@ fping_menu() {
     case ${FPING_CHOICE} in
           1)
                 echo
+                check_fping
                 check_single_host
                 ;;
           2)
                 echo
+                check_fping
                 check_multi_host
                 ;;
           3)
@@ -61,5 +69,6 @@ fping_menu() {
                 ;;
      esac
 }
+
 
 fping_menu
